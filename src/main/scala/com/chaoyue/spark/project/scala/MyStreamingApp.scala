3bb1365250e0082e92280807864177f69013d46d
fangmiao@MACBook <1816232121@qq.com>
2019-04-20 20:11:49 +0800

    190420

--- src/main/scala/com/chaoyue/spark/project/scala/MyStreamingApp.scala
+++ src/main/scala/com/chaoyue/spark/project/scala/MyStreamingApp.scala
@@ -69,7 +69,7 @@ object MyStreamingApp {
 
     //使用Window操作计算窗口大小为30分钟的访问情况，分类目统计
     cleanData.map(x => {
-      (x.time.substring(0,8)+"_"+x.courseId, 1)
+      (x.time.substring(0,8)+"_"+x.courseId, 1)//0点附近有问题
     }).reduceByKeyAndWindow((x: Int, y: Int) => x + y,
       Seconds(1800), Seconds(1800)).foreachRDD(rdd => {
       rdd.foreachPartition(partitionRecords => {
